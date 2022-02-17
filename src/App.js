7613eab6c2871b1ee5c7cc25f2a85e8ca3697f26
clayton perszyk <claytonperszyk@claytons-Mac-mini.local>
2022-02-17 15:59:15 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [companies, setCompanies] = useState([]);
+  const [isLoaded, setIsLoaded ]= useState(false);
+
+  async function  getCompanies() {
+    const baseURL = 'https://fakerapi.it'
+    
+    try {
+      const response = await fetch(`${baseURL}/api/v1/companies`);
+      const companiesJSON = await response.json();
+      setCompanies(companiesJSON);
+      setIsLoaded(true);
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
+  useEffect(() => {
+    getCompanies();
+  }, []);
+
   return (
     <div className="app">
-      {/* Your code starts here */}
-      
+     <button onClick={getCompanies}>Refresh</button>
+      {!isLoaded && (<div >loading...</div>)}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Contact</th>
+            <th>Country</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+         <tbody>
+            {companies.data && companies.data.map((company) =>{
+              return (
+                <tr key={company.id}>
+                  <td>{company.name}</td>
+                  <td>{`${company.contact.firstname} ${company.contact.lastname}`}</td>
+                  <td>{company.country}</td>
+                  <td><a href={company.website}>{company.website}</a></td>
+                </tr>
+              );
+            })}
+         </tbody>
+      </table>
     </div>
   );
 }
